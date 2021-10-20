8475a5d11f9226b4be15e8da9ac86057d7478413
BEJOSCHR <benjamin@schranni.de>
2021-10-20 23:27:13 +0200

    Added main class and package

---
+++ Bomberfrau/src/uni/bombenstimmung/de/main/Bomberfrau_Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package uni.bombenstimmung.de.main;
+
+/**
+ * @author BEJOSCH
+ *
+ */
+public class Bomberfrau_Main {
+
+	/**
+	 * The start of everything (in this program)
+	 * @param args The arguments which are added to the start argument
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Starting Bomberfrau, juhu!");
+		
+	}
+
+}
