e4a9ec0f2f55ee1b8a9f8d90b4c7d6be1ecc65b1
BEJOSCHR <benjamin@schranni.de>
2021-10-21 13:12:59 +0200

    Added example methode and test output

--- Bomberfrau/src/uni/bombenstimmung/de/main/Bomberfrau_Main.java
+++ Bomberfrau/src/uni/bombenstimmung/de/main/Bomberfrau_Main.java
@@ -17,6 +17,31 @@ public class Bomberfrau_Main {
 		
 		System.out.println("Starting Bomberfrau, juhu!");
 		
+		System.out.println();
+		System.out.println("Test: ");
+		for(int i = 99 ; i > 0 ; i--) {
+			if(i%9 == 0 && i != 0 && i != 99) {
+				System.out.println();
+			}
+			System.out.print(doubleWriteNumber(i)+" ");
+		}
+		System.out.println();
+		
 	}
 
+	//Beispiel wie ich mir eine Methode mit Documentation etc vorstellen würde:
+	
+	/**
+	 * Stellt jede nummer mit min. 2 Stellen dar
+	 * @param number
+	 * @return Die nummer aber immer in min 2 Stellen
+	 */
+	private static String doubleWriteNumber(int number) {
+		if(number >= 10) {
+			return ""+number;
+		}else {
+			return "0"+number;
+		}
+	}
+	
 }
