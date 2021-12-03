dded13c0ad5e604120d1b10745930ff261ba291a
Tim Westerholz <timwesterholz@gmail.com>
2021-12-03 14:49:09 +0100

    Implemented basic UDP-Server-Client

--- Bomberfrau/src/main/java/uni/bombenstimmung/de/backend/serverconnection/ConnectionData.java
+++ Bomberfrau/src/main/java/uni/bombenstimmung/de/backend/serverconnection/ConnectionData.java
@@ -4,10 +4,11 @@
  * Version 1.0
  * Author: Benni
  *
- * Enthält alle Daten der Server-Client-Verbindung
+ * Enthï¿½lt alle Daten der Server-Client-Verbindung
  */
 package uni.bombenstimmung.de.backend.serverconnection;
 
 public class ConnectionData {
-
+	public static final int PORT = 23456;
+	public static final String IP = "127.0.0.1";	
 }
--- Bomberfrau/src/main/java/uni/bombenstimmung/de/main/BomberfrauMain.java
+++ Bomberfrau/src/main/java/uni/bombenstimmung/de/main/BomberfrauMain.java
@@ -14,15 +14,16 @@ import uni.bombenstimmung.de.backend.console.MessageType;
 import uni.bombenstimmung.de.backend.graphics.GraphicsHandler;
 import uni.bombenstimmung.de.backend.images.ImageHandler;
 import uni.bombenstimmung.de.backend.maa.MouseActionAreaHandler;
+import uni.bombenstimmung.de.backend.serverconnection.host.ConnectedClient;
 
 public class BomberfrauMain {
 	
-	public static final String AUTHOR = "Bombenstimmung - Uni Wuppertal - ©2021";
+	public static final String AUTHOR = "Bombenstimmung - Uni Wuppertal - ï¿½2021";
 	public static final String VERSION = "Alpha - V0.0.6";
 	
 	/**
 	 * Der Start von allem (in diesem Projekt)
-	 * @param args Die Argumente die dem Start mitgegeben werden können
+	 * @param args Die Argumente die dem Start mitgegeben werden kï¿½nnen
 	 */
 	public static void main(String[] args) {
 		
@@ -39,6 +40,12 @@ public class BomberfrauMain {
 		ImageHandler.initImages();
 		ConsoleHandler.startInputScanner();
 		
+		ConnectedClient host = new ConnectedClient(true, 0);
+		System.out.println("Client: " + host.getId()+" Host: " +host.isHost());
+		ConnectedClient client = new ConnectedClient(false, 1);
+		System.out.println("Client: " + client.getId()+" Host: " +client.isHost());
+		ConnectedClient client2 = new ConnectedClient(false, 2);
+		System.out.println("Client: " + client2.getId()+" Host: " +client2.isHost());
 		//2. ...
 		//TODO
 		
