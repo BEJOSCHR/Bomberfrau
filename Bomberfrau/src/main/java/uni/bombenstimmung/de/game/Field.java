bb295d5963049b55ea2985bb55d34d9458796801
Dennis <Dennis@Laptop.de>
2021-12-16 10:31:54 +0100

    Weiterer Fortschritt beim Bau des Feldes

--- Bomberfrau/src/main/java/uni/bombenstimmung/de/game/Field.java
+++ Bomberfrau/src/main/java/uni/bombenstimmung/de/game/Field.java
@@ -6,11 +6,53 @@
 package uni.bombenstimmung.de.game;
 
 import java.awt.Graphics;
+
+import uni.bombenstimmung.de.backend.console.ConsoleHandler;
+
 import java.awt.Color;
 
 public class Field extends Entity {
 
     private FieldContent content;
+    
+    public Field(int x, int y, FieldContent cont) {
+	super.xPosition = x;
+	super.yPosition = y;
+	content = cont;
+    }
+    
+    public static String getFieldTypeRepresentation(FieldContent type) {
+	switch(type) {
+		case BLOCK:
+			return "BL";
+		case BORDER:
+			return "BO";
+		case EMPTY:
+			return "EM";
+		case WALL:
+			return "WA";
+		default:
+			ConsoleHandler.print("Unknown fieldtype '"+type+"'!");
+			return "##";
+	}
+    }
+    
+    public static FieldContent getFieldTypeFromRepresentation(String representation) {
+	
+	switch(representation) {
+		case "BL":
+		    return FieldContent.BLOCK;
+		case "BO":
+		    return FieldContent.BORDER;
+		case "EM":
+		    return FieldContent.EMPTY;
+		case "WA":
+		    return FieldContent.WALL;
+		default:
+		    ConsoleHandler.print("Unknown fieldtype representation '"+representation+"'!");
+		    return FieldContent.EMPTY;
+	}
+    }
     
     public void drawField(Graphics g, int x, int y, FieldContent cont) {
 	switch (cont) {
--- Bomberfrau/src/main/java/uni/bombenstimmung/de/game/FieldContent.java
+++ Bomberfrau/src/main/java/uni/bombenstimmung/de/game/FieldContent.java
@@ -13,43 +13,5 @@ public enum FieldContent {
     UPGRADE,
     EMPTY,
     BLOCK;
-	
-	
-	
-public static String getFieldTypeRepresentation(FieldContent type) {
-		
-		switch(type) {
-		case BLOCK:
-			return "BL";
-		case BORDER:
-			return "BO";
-		case EMPTY:
-			return "EM";
-		case WALL:
-			return "WA";
-		default:
-			ConsoleHandler.print("Unknown fieldtype '"+type+"'!");
-			return "##";
-		}
-		
-	}
+}	
 
-
-public static FieldContent getFieldTypeFromRepresentation(String representation) {
-	
-	switch(representation) {
-	case "BL":
-		return BLOCK;
-	case "BO":
-		return BORDER;
-	case "EM":
-		return EMPTY;
-	case "WA":
-		return WALL;
-	default:
-		ConsoleHandler.print("Unknown fieldtype representation '"+representation+"'!");
-		return EMPTY;
-	}
-	
-}
-}
