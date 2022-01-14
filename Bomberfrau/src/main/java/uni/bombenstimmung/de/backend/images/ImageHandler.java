26539b26397504e3f0ac17cc21fb0e321a487083
TemeIm <lutschmeinjaggon@gmail.com>
2022-01-14 13:01:44 +0100

    Bilder eingepflegt in ImageHandler

--- Bomberfrau/src/main/java/uni/bombenstimmung/de/backend/images/ImageHandler.java
+++ Bomberfrau/src/main/java/uni/bombenstimmung/de/backend/images/ImageHandler.java
@@ -88,7 +88,14 @@ public class ImageHandler {
 	    	new LoadedImage("Ingame/bombergirl_walk_1.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_1, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
 	    	new LoadedImage("Ingame/bombergirl_dead.png", ImageType.IMAGE_INGAME_CHARACTER_DEAD, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
 		ConsoleHandler.print("Loaded images ("+images.size()+")", MessageType.BACKEND);
-		
+		new LoadedImage("Ingame/Priest_Walk_1.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_1, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_2.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_2, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_Side_Left_1.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_SIDE_LEFT_1, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_Side_Left_Idle.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_SIDE_LEFT_IDLE, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_Side_Right_1.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_SIDE_RIGHT_1, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_Side_Right_Idle.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_SIDE_RIGHT_IDLE, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_North_1.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_NORTH_1, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
+		new LoadedImage("Ingame/Priest_Walk_North_Idle.png", ImageType.IMAGE_INGAME_CHARACTER_WALK_NORTH_IDLE, GameData.FIELD_DIMENSION, GameData.FIELD_DIMENSION);
 	}
 	
 	/**
