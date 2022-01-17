6539eb702530aa99eaca2860e5e0274bf5def9d9
alexe <alexe@ACER-Alexej.mshome.net>
2022-01-17 15:29:58 +0100

    LeftText

--- Bomberfrau/src/main/java/uni/bombenstimmung/de/backend/graphics/Label.java
+++ Bomberfrau/src/main/java/uni/bombenstimmung/de/backend/graphics/Label.java
@@ -135,10 +135,10 @@ public class Label extends JLabel {
 			g.setColor(Color.WHITE);
 			Color gold = new Color(255,204,51);
 			GraphicsHandler.drawCentralisedText(g, Color.BLACK, 100, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_TITEL).getContent(), GraphicsHandler.getWidth()/2, GraphicsHandler.getHeight()/16);
-			GraphicsHandler.drawCentralisedText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_1).getContent(), GraphicsHandler.getWidth()/2, GraphicsHandler.getHeight()*2/8);
-			GraphicsHandler.drawCentralisedText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_2).getContent(), GraphicsHandler.getWidth()/2, GraphicsHandler.getHeight()*3/8);
-			GraphicsHandler.drawCentralisedText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_3).getContent(), GraphicsHandler.getWidth()/2, GraphicsHandler.getHeight()*4/8);
-			GraphicsHandler.drawCentralisedText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_4).getContent(), GraphicsHandler.getWidth()/2, GraphicsHandler.getHeight()*5/8);
+			GraphicsHandler.drawLeftText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_1).getContent(), GraphicsHandler.getWidth()*1/4, GraphicsHandler.getHeight()*2/8);
+			GraphicsHandler.drawLeftText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_2).getContent(), GraphicsHandler.getWidth()*1/4, GraphicsHandler.getHeight()*3/8);
+			GraphicsHandler.drawLeftText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_3).getContent(), GraphicsHandler.getWidth()*1/4, GraphicsHandler.getHeight()*4/8);
+			GraphicsHandler.drawLeftText(g, gold.darker(), 70, LanguageHandler.getLLB(LanguageBlockType.LB_AFTERGAME_RANKING_4).getContent(), GraphicsHandler.getWidth()*1/4, GraphicsHandler.getHeight()*5/8);
 			break;
 		default:
 			ConsoleHandler.print("Illegal displayType! Can't draw for type '"+GraphicsHandler.getDisplayType()+"'", MessageType.ERROR);
